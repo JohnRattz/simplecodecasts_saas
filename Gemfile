8d5c4e59849693de23b5c8ac752fba1b20c2ac4c
John Rattz <jrattz94@gmail.com>
2017-11-26 19:04:46 -0500

    Replaced removed old functions with their replacements; added jQuery to application.html.erb

--- Gemfile
+++ Gemfile
@@ -51,7 +51,8 @@ gem 'figaro' # '1.0.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
+#, '~> 3.1.11'
 # Use inicorn as the app server
 # gem 'unicorn'
 # Use Capistrano for deployment
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   def save_with_payment
     if valid?
       # Use Stripe gem to register user for a plan and charge them simultaneously.
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
